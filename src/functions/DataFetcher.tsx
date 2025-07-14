48abd774b39bcebeee47be9ef9c9cd4b5ac91721
jruano-espol <jruano@espol.edu.ec>
2025-07-14 23:23:42 +0000

    LocalStorage

--- src/functions/DataFetcher.tsx
+++ src/functions/DataFetcher.tsx
@@ -7,6 +7,11 @@ export interface DataFetcherOutput {
     error: string | null;
 }
 
+interface CachedItem {
+    data: OpenMeteoResponse;
+    timestamp: number;
+}
+
 const ciudades: Record<string, { lat: number, lon: number }> = {
   guayaquil: {lat: -2.170997,lon: -79.922359},
   quito: {lat: -0.180653, lon: -78.467838},
@@ -19,6 +24,13 @@ export default function DataFetcher(ciudad: string): DataFetcherOutput {
     let [loading, setLoading] = useState(true);
     let [error, setError] = useState<string|null>(null);
 
+    const UPDATE_TIMEOUT_MS = 5 * 60 * 1000; // 5 minutes
+
+    function getCachedItem(ciudad: string): CachedItem | null {
+        let str = localStorage.getItem(ciudad);
+        return str ? JSON.parse(str) : null;
+    }
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -26,11 +38,20 @@ export default function DataFetcher(ciudad: string): DataFetcherOutput {
                     throw new Error(`La ciudad "${ciudad}" es inválida. Debe ser una de las siguientes: ${Object.keys(ciudades).join(", ")}`);
                 }
                 const url = `https://api.open-meteo.com/v1/forecast?latitude=${ciudades[ciudad].lat}&longitude=${ciudades[ciudad].lon}&hourly=temperature_2m,wind_speed_10m&current=temperature_2m,relative_humidity_2m,wind_speed_10m,apparent_temperature&timezone=America%2FChicago`;
-                const response = await fetch(url);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
+                const now = Date.now();
+                
+                let cached_response = getCachedItem(ciudad);
+                if (!cached_response || now > cached_response.timestamp + UPDATE_TIMEOUT_MS) {
+                    const response = await fetch(url);
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    const data = await response.json();
+                    const item = { data: data, timestamp: now };
+                    localStorage.setItem(ciudad, JSON.stringify(item));
+                    cached_response = item;
                 }
-                setData(await response.json());
+                setData(cached_response.data);
             } catch (error: Error | unknown) {
                 if (error instanceof Error) {
                     setError(error.message);
